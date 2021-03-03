4dda1f438f11178210289a6191a5c5a6d4bc08ad
Cihat <cihatertem@gmail.com>
2021-03-03 16:55:28 +0300

    Django-Blog 7. Commit

--- blog/settings.py
+++ blog/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ['SECRET_KEY'] # Instead of your actual secret key
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["fincan.pythonanywhere.com"]
 
